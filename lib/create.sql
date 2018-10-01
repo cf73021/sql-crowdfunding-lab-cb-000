d3abb5f2077cd0d3958af6adf50e34d4f9d88a78
Chris Franklin <cf73021@gmail.com>
2018-10-01 00:43:02 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE project (
+id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal INTEGER
+start_date DATE
+end_date DATE
+);
+
+CREATE TABLE user (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER
+);
+
+CREATE TABLE pledge (
+id INTEGER PRIMARY KEY,
+amount INTEGER,
+age INTEGER,
+breed TEXT
+);
