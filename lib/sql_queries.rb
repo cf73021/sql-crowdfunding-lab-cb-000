a49fc64199f90c601586a9fe4b5f1f8281537c84
Chris Franklin <cf73021@gmail.com>
2018-10-01 00:57:40 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,11 +7,11 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT projects.title, pledges.amount
+"SELECT DISTINCT projects.title, pledges.amount
 FROM projects
 INNER JOIN pledges ON projects.id = pledges.project_id
 INNER JOIN users ON users.id = pledges.user_id
-ORDER BY users.name 
+ORDER BY users.name
 "
 end
 
